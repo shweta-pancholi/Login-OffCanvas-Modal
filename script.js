f62f826a1d1b6c1d7e282f8d404e0f6cdff567e7
Shweta Pancholi <152668671+shweta-pancholi@users.noreply.github.com>
2026-05-22 07:37:25 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector(".btn1")
+let loginDiv = document.querySelector(".login-form");
+
+btn.addEventListener("click", ()=>{
+    loginDiv.classList.toggle("active")
+})
+
