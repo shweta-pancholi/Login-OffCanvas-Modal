6ac19d81a9b6d1d038070bd304f755daa7af8e5e
Shweta Pancholi <152668671+shweta-pancholi@users.noreply.github.com>
2026-05-22 08:17:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ btn.addEventListener("click", ()=>{
     loginDiv.classList.toggle("active")
 })
 
+let spantag = document.querySelector(".form-cut")
+
+spantag.addEventListener("click", ()=>{
+    loginDiv.classList.remove("active")
+})
+
